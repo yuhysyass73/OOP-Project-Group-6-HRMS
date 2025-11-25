6b29b439333ea8750840ee6fb873eb8d51a0f29b
Dao An Huy <anhuy73717@gmail.com>
2025-11-25 10:26:58 +0700

    Phong Ban

--- src/PhongBan.java
+++ src/PhongBan.java
@@ -14,4 +14,18 @@ public class PhongBan {
     public String getTenPhongBan() {
         return tenPhongBan;
     }
+
+    public void setMaPhongBan(String maPhongBan) {
+        this.maPhongBan = maPhongBan;
+    }
+
+    public void setTenPhongBan(String tenPhongBan) {
+        this.tenPhongBan = tenPhongBan;
+    }
+
+    @Override
+    public String toString() {
+        
+        return tenPhongBan; 
+    }
 }
