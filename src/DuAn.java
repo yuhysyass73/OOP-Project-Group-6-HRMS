a4adbbef7132da157bbe72a87447da22a1993f10
Phi Hoang <ngphihoang06@gmail.com>
2025-12-02 09:37:54 +0700

    Update constructor DuAn

--- src/DuAn.java
+++ src/DuAn.java
@@ -5,6 +5,15 @@ public class DuAn {
 
     private String maDuAn;
     private String tenDuAn;
-    private int doPhucTap; // 1 2 3
-    
+    private int doPhucTap; // thang 1 2 3
+
+    private List<NhanVien> danhSachThanhVien;
+
+    public DuAn(String maDuAN, String tenDuAn, int doPhucTap)
+    {
+        this.maDuAn = maDuAn;
+        this.tenDuAn = tenDuAn;
+        this.doPhucTap = doPhucTap;
+        this.danhSachThanhVien = new ArrayList<>();
+    }
 }
