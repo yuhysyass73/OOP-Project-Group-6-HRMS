c89f722722e78c4833e4c3120e3ec9232c700759
Dao An Huy <anhuy73717@gmail.com>
2025-12-24 10:56:10 +0700

    thiết lập cấu hình tab email

--- src/tabs/TabEmail.java
+++ src/tabs/TabEmail.java
@@ -12,6 +12,19 @@ import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.*;
 
-public class TabEmail {
-    
+public class TabEmail extends JPanel {
+
+    private QuanLyNhanVienGUI parent;
+    private List<NhanVien> danhSachNV;
+
+    private JTable tableNV;
+    private DefaultTableModel modelNV;
+    private JTextField txtTieuDe;
+    private JTextArea txtNoiDung;
+    private JPasswordField txtMatKhauEmail;
+    private JTextField txtEmailGui;
+    private JProgressBar progressBar;
+    private JLabel lblStatus;
+
+    private JButton btnGui;
 }
