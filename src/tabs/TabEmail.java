7f5ae08fcb3c430164e4070725af97b8455ec016
Dao An Huy <anhuy73717@gmail.com>
2025-12-24 10:50:48 +0700

    thêm tab email

---
+++ src/tabs/TabEmail.java
@@ -0,0 +1,17 @@
+package tabs;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+import MainApp.*;
+import doituong.*;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class TabEmail {
+    
+}
